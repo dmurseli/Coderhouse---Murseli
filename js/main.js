4323d30fafbcfb7747aee7e671ac4a23bbc956ab
Dario Murseli <dmurseli@hotmail.com>
2023-03-29 19:59:49 -0300

    1.0.0
    Armando el proyecto de la primaera entrega.
    Acondicionando el for y probando condicionales. Validando el valorSesion

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
-// Simulador interactivo - ENTREGA DEL PROYECTO FINAL - Aspectos a incluir
+// Crear un algoritmo con un condicional
+// Crear un algoritmo utilizando un ciclo
+// Armar un simulador interactivo, la estructura final de tu proyecto integrador
 
-// ✓ Archivo HTML y Archivo JS, referenciado en el HTML por etiqueta <script src="js/miarchivo.js"></script>,
-// que incluya la definición de un algoritmo en JavaScript que emplee funciones para resolver el procesamiento principal del simulador
-
-// Ejemplo
-// ✓ Calcular costo total de productos y/o servicios seleccionados por el usuario.
-// ✓ Calcular pagos en cuotas sobre un monto determinado.
-// ✓ Calcular valor final de un producto seleccionado en función de impuestos y descuentos.
-// ✓ Calcular tiempo de espera promedio en relación con la cantidad de turnos registrados.
-// ✓ Calcular edad promedio de personas registradas.
-// ✓ Calcular nota final de alumnos ingresados.
-
-let paciente = "";
-let dia = "";
-let hora = "";
-let valorSesion = "";
+// let paciente = "";
+// let dia = "";
+// let hora = "";
+// let valorSesion;
 
 
-for (let index = 0; index < 2; index++) {
-    paciente = prompt("Por favor, ingresá el nombre del PACIENTE:");
-    console.log();
-    dia = prompt("Por favor, ingresá el DIA DE LA SESIÓN:");
-    hora = prompt("Por favor, ingresá la HORA DE LA SESIÓN:");
-    valorSesion = parseInt(prompt("Por favor, ingresá el VALOR DE LA SESIÓN:"));
+// for (let index = 1; index < 4; index++) {
+//     paciente = prompt("Por favor, ingresá el nombre del PACIENTE:");
+//     console.log("El paciente número " + index + " es: " + paciente);
+//     dia = prompt("Por favor, ingresá el DIA DE LA SESIÓN:");
+//     console.log("El día asignado es: " + dia);
+//     hora = prompt("Por favor, ingresá la HORA DE LA SESIÓN:");
+//     console.log("La hora o turno asignado es: " + hora);
+//     do{
+
+//         valorSesion = parseInt(prompt("Por favor, ingresá el VALOR DE LA SESIÓN:"));
+//         if(isNaN(valorSesion)){
+//             alert("Eso no es un número, por favor ingrese un número válido")
+//         }
+//     }
+//         while (isNaN(valorSesion) );
+//         console.log("El valor acordado es: " + valorSesion);
     
-};
+// };
+
+//  console.log("Finalizó la carga");
